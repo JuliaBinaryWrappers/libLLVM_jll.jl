978fb065041731e2e3a1e6d4a773fbba0f56c760
jlbuild <juliabuildbot@gmail.com>
2026-02-06 01:06:06 +0000

    libLLVM_jll build 15.0.7+11

--- src/libLLVM_jll.jl
+++ src/libLLVM_jll.jl
@@ -6,5 +6,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libLLVM")
-JLLWrappers.@generate_main_file("libLLVM", UUID("8f36deef-c2a5-5394-99ed-8e07531fb29a"))
+JLLWrappers.@generate_main_file("libLLVM", Base.UUID("8f36deef-c2a5-5394-99ed-8e07531fb29a"))
 end  # module libLLVM_jll
